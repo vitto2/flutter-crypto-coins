2256c599ea2bb888fb7d28d08b62897a3131cc40
vitor2071 <ext_vitsouza@ml.com>
2022-09-27 13:11:54 -0300

    criando appbar dinâmica

--- lib/view/moedas_page.dart
+++ lib/view/moedas_page.dart
@@ -12,24 +12,14 @@ class MoedasPage extends StatefulWidget {
 
 class _MoedasPageState extends State<MoedasPage> {
   final tabela = CoinRepository.table;
-
   List<Moeda> selectedItem = [];
-
+  int selectedQtd = 0;
   NumberFormat real = NumberFormat.currency(locale: 'pt-BR', name: 'R\$');
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "Criptomoedas",
-          style: TextStyle(
-            color: Colors.black,
-          ),
-        ),
-        centerTitle: true,
-        backgroundColor: Colors.white,
-      ),
+      appBar: myAppBar(),
       body: ListView.separated(
           itemBuilder: (BuildContext context, int index) {
             return ListTile(
@@ -56,6 +46,7 @@ class _MoedasPageState extends State<MoedasPage> {
                   selectedItem.remove(tabela[index]);
                 } else {
                   selectedItem.add(tabela[index]);
+                  selectedQtd++;
                 }
                 setState(() {});
               },
@@ -68,5 +59,33 @@ class _MoedasPageState extends State<MoedasPage> {
               ),
           itemCount: tabela.length),
     );
+  }
+
+  myAppBar() {
+    if (selectedItem.isEmpty) {
+      return AppBar(
+        leading: Text(
+          "Quantidade $selectedQtd",
+          style: const TextStyle(color: Colors.white),
+        ),
+        title: const Text(
+          "Criptomoedas",
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+      );
+    } else {
+      return AppBar(
+        leading: IconButton(
+          onPressed: () {
+            selectedItem = [];
+          },
+          icon: const Icon(Icons.arrow_back_ios),
+        ),
+        backgroundColor: Colors.white54,
+        title: Text("${selectedItem.length} selecionados"),
+      );
+    }
   }
 }
